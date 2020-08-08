cb11c87fb12e807aa3dd2086ec474dd8036e8ce9
min.zeng <min.zen@qq.com>
2020-08-08 11:54:40 +0800

    add netty feature and taskqueue

--- src/main/java/com/zm/netty/quictstarttcp/NettyServer.java
+++ src/main/java/com/zm/netty/quictstarttcp/NettyServer.java
@@ -2,6 +2,7 @@ package com.zm.netty.quictstarttcp;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -16,14 +17,16 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 public class NettyServer {
 
     public static void main(String[] args) throws InterruptedException {
-        // 创建BossGroup  和  WorkGroup
-        /**
+        /*
+         * 创建BossGroup  和  WorkGroup
+         *
          * 创建两个线程组  boos和work
          * boos只处理连接请求  真正和客户端业务处理 只交给workGroup处理
          * 两个都是无限循环
          */
-        NioEventLoopGroup boosGroup = new NioEventLoopGroup();
-        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+        // 默认线程数CPU核心数 * 2  DEFAULT_EVENT_LOOP_THREADS = Math.max(1, SystemPropertyUtil.getInt("io.netty.eventLoopThreads", NettyRuntime.availableProcessors() * 2));
+        NioEventLoopGroup boosGroup = new NioEventLoopGroup(1); // 这里线程组中只要一个线程
+        NioEventLoopGroup workGroup = new NioEventLoopGroup(8);
 
         try {
 
@@ -46,6 +49,20 @@ public class NettyServer {
 
             // 绑定端口
             ChannelFuture channelFuture = serverBootstrap.bind(6666).sync();
+
+            // 给channelFuture注册监听器  监听我们关系的事件
+            channelFuture.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                    if (channelFuture.isSuccess()) {
+                        System.out.println("绑定端口成功！");
+                    }
+                    if (channelFuture.isCancelled()) {
+                        System.out.println("绑定端口失败！");
+                        System.out.println("异常信息：" + channelFuture.cause().toString());
+                    }
+                }
+            });
 
             // 对关闭通道进行监听 异步
             channelFuture.channel().closeFuture().sync();
--- src/main/java/com/zm/netty/quictstarttcp/NettyServerHandler.java
+++ src/main/java/com/zm/netty/quictstarttcp/NettyServerHandler.java
@@ -6,6 +6,8 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Describle This Class Is 自定义handler  继承handlerAdapter
  * @Author ZengMin
@@ -15,7 +17,7 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
 
 
     /**
-     * 通道读写方法  可以去到客户端发送的数据
+     * 通道读写方法  可以取到客户端发送的数据
      *
      * @param ctx 上下文对象  有pipline channel
      * @param msg 客户端发送的数据
@@ -23,11 +25,30 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
      */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // 提交任务
+        // 如果这里很耗时的逻辑  需要放入eventloop线程池taskQueue里面
+        ctx.channel().eventLoop().execute(() -> {   // 提交到channel对应的eventloop的taskQueue中
+            ByteBuf byteBuf = (ByteBuf) msg;
+            System.out.println("1客户端：IP:" + ctx.channel().remoteAddress() + "----" + byteBuf.toString(CharsetUtil.UTF_8) + "-----" + Thread.currentThread().getName());
+        });
+
+        // 后提交的排队执行
+        ctx.channel().eventLoop().execute(() -> {   // 提交到channel对应的eventloop的taskQueue中
+            ByteBuf byteBuf = (ByteBuf) msg;
+            System.out.println("2客户端：IP:" + ctx.channel().remoteAddress() + "----" + byteBuf.toString(CharsetUtil.UTF_8) +  "-----" + Thread.currentThread().getName());
+        });
+
+
+        // 提交定时任务 是提交到 scheduleTaskQueue中
+        ctx.channel().eventLoop().schedule(() -> {
+            ByteBuf byteBuf = (ByteBuf) msg;
+            System.out.println("3 schedule 客户端：IP:" + ctx.channel().remoteAddress() + "----" + byteBuf.toString(CharsetUtil.UTF_8) +  "-----" + Thread.currentThread().getName());
+        }, 10, TimeUnit.SECONDS);
 
         // netty的ByteBuffer 性能更好
         ByteBuf byteBuf = (ByteBuf) msg;
         // 打印客户端数据
-        System.out.println("客户端：IP:" + ctx.channel().remoteAddress() + "----" + byteBuf.toString(CharsetUtil.UTF_8));
+        System.out.println("客户端：IP:" + ctx.channel().remoteAddress() + "----" + byteBuf.toString(CharsetUtil.UTF_8) +  "-----" + Thread.currentThread().getName());
     }
 
 
@@ -40,7 +61,7 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
         // 向客户端回送响应数据
-        ctx.writeAndFlush(Unpooled.copiedBuffer("hello!", CharsetUtil.UTF_8));
+        ctx.writeAndFlush(Unpooled.copiedBuffer("hello complete!", CharsetUtil.UTF_8));
     }
 
 
